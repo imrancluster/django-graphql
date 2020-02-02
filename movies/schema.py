575cb836e869ee397be6f64e7e094713ffd80b29
Imran Sarder <imrancluster@gmail.com>
2020-02-02 15:33:32 +0600

    initial commit with basic example of graphql with django

---
+++ movies/schema.py
@@ -0,0 +1,208 @@
+import graphene
+from django.db.models import Q
+from graphene_django.types import DjangoObjectType, ObjectType
+from movies.models import Actor, Movie
+
+
+# Create a GraphQL type for the actor model
+from movies.utils import get_paginator
+
+
+class ActorType(DjangoObjectType):
+    class Meta:
+        model = Actor
+
+
+# Create a GraphQL type for the movie model
+class MovieType(DjangoObjectType):
+    class Meta:
+        model = Movie
+
+
+# Now we create a corresponding PaginatedType for that object type:
+class MoviePaginatedType(graphene.ObjectType):
+    page = graphene.Int()
+    pages = graphene.Int()
+    has_next = graphene.Boolean()
+    has_prev = graphene.Boolean()
+    objects = graphene.List(MovieType)
+
+
+# Create a Query type
+class Query(ObjectType):
+
+    actor = graphene.Field(ActorType, id=graphene.Int())
+    movie = graphene.Field(MovieType, id=graphene.Int())
+    actors = graphene.List(ActorType)
+    movies= graphene.List(MovieType)
+
+    # Add the first and skip parameters
+    movielist = graphene.List(
+        MovieType,
+        search=graphene.String(),
+        first=graphene.Int(),
+        skip=graphene.Int(),
+    )
+
+    # Testing another pagination using django core pagination
+    allmovies = graphene.Field(MoviePaginatedType, page=graphene.Int())
+
+
+    def resolve_actor(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Actor.objects.get(pk=id)
+
+        return None
+
+    def resolve_movie(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Movie.objects.get(pk=id)
+
+        return None
+
+    def resolve_actors(self, info, **kwargs):
+        return Actor.objects.all()
+
+    def resolve_movies(self, info, **kwargs):
+        return Movie.objects.all()
+
+    def resolve_movielist(self, info, search=None, first=None, skip=None, **kwargs):
+        qs = Movie.objects.all()
+
+        if search:
+            filter = (
+                    Q(title__icontains=search) |
+                    Q(year__iexact=int(search))
+            )
+            qs = qs.filter(filter)
+
+        if skip:
+            qs = qs[skip:]
+
+        if first:
+            qs = qs[:first]
+
+        return qs
+
+    # Now, in your resolver functions, you just query your objects and turn the queryset into the PaginatedType using the helper function:
+    def resolve_allmovies(self, info, page):
+        page_size = 2
+        qs = Movie.objects.all()
+        return get_paginator(qs, page_size, page, MoviePaginatedType)
+
+
+# Create Input Object Types
+class ActorInput(graphene.InputObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+
+
+class MovieInput(graphene.InputObjectType):
+    id = graphene.ID()
+    title = graphene.String()
+    actors = graphene.List(ActorInput)
+    year = graphene.Int()
+
+
+# Create mutations for actors
+class CreateActor(graphene.Mutation):
+    class Arguments:
+        input = ActorInput(required=True)
+
+    ok = graphene.Boolean()
+    actor = graphene.Field(ActorType)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        ok = True
+        actor_instance = Actor(name=input.name)
+        actor_instance.save()
+        return CreateActor(ok=ok, actor=actor_instance)
+
+
+class UpdateActor(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        input = ActorInput(required=True)
+
+    ok = graphene.Boolean()
+    actor = graphene.Field(ActorType)
+
+    @staticmethod
+    def mutate(root, info, id, input=None):
+        ok = False
+        actor_instance = Actor.objects.get(pk=id)
+        if actor_instance:
+            ok = True
+            actor_instance.name = input.name
+            actor_instance.save()
+            return UpdateActor(ok=ok, actor=actor_instance)
+        return UpdateActor(ok=ok, actor=None)
+
+
+# Create mutations for movies
+class CreateMovie(graphene.Mutation):
+    class Arguments:
+        input = MovieInput(required=True)
+
+    ok = graphene.Boolean()
+    movie = graphene.Field(MovieType)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        ok = True
+        actors = []
+        for actor_input in input.actors:
+          actor = Actor.objects.get(pk=actor_input.id)
+          if actor is None:
+            return CreateMovie(ok=False, movie=None)
+          actors.append(actor)
+        movie_instance = Movie(
+          title=input.title,
+          year=input.year
+          )
+        movie_instance.save()
+        movie_instance.actors.set(actors)
+        return CreateMovie(ok=ok, movie=movie_instance)
+
+
+class UpdateMovie(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        input = MovieInput(required=True)
+
+    ok = graphene.Boolean()
+    movie = graphene.Field(MovieType)
+
+    @staticmethod
+    def mutate(root, info, id, input=None):
+        ok = False
+        movie_instance = Movie.objects.get(pk=id)
+        if movie_instance:
+            ok = True
+            actors = []
+            for actor_input in input.actors:
+              actor = Actor.objects.get(pk=actor_input.id)
+              if actor is None:
+                return UpdateMovie(ok=False, movie=None)
+              actors.append(actor)
+            movie_instance.title = input.title
+            movie_instance.year = input.year
+            movie_instance.save()
+            movie_instance.actors.set(actors)
+            return UpdateMovie(ok=ok, movie=movie_instance)
+        return UpdateMovie(ok=ok, movie=None)
+
+
+class Mutation(graphene.ObjectType):
+    create_actor = CreateActor.Field()
+    update_actor = UpdateActor.Field()
+    create_movie = CreateMovie.Field()
+    update_movie = UpdateMovie.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
